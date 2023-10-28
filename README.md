55944fd915a0ee81a0969a454beb3003e3ae4dd8
Corry Frydlewicz <cfrydlewicz@gmail.com>
2023-10-27 18:32:30 -0700

    Initial Commit
    Basic ideas worked out.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # corry_resume
- My HTML Resume
+My HTML Resume
+
+## Why HTML?
+I believe HTML is _the best medium_ for communication currently available to humans. It's open source, is capabile of extreme precision, and allows for communicating through nearly any visual and audal medium. It's also potentially the most accessible medium across any device, allowing for assistive tools, and easily crawled by tools that translate it into different languages. When written semantically, it even does an amazing job at communicating intentions.
+
+## Why Git?
+Transparency is one of my core values, so I offer visibility to my resume's version history to anyone who has the time and interest to look at it.
---
+++ dev/Gruntfile.js
@@ -0,0 +1,66 @@
+/*jshint esversion: 6 */
+module.exports = function(grunt) {
+
+  const sass = require('node-sass');
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    devUpdate: {
+      main: {
+        options: {
+          updateType: 'report', // just report outdated packages
+          reportUpdated: false, // don't report up-to-date packages
+        }
+      }
+    },
+
+    jshint: {
+      all: ['Gruntfile.js']
+    },
+
+    sass: {
+      options: {
+        implementation: sass,
+        sourceMap: false,
+        style: 'expanded'
+      },
+      dist: {
+        files: {
+          'css/resume.concat.css' : 'scss/00_manifest.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version']
+      },
+      your_target: {
+        src: 'css/*.concat.css'
+      },
+    },
+
+    cssmin: {
+      mintheme: {
+        src: 'css/resume.concat.css',
+        dest: 'css/resume.min.css'
+      }
+    },
+
+    watch: {
+      files: [
+        'Gruntfile.js',
+        'package.json',
+        'scss/*.scss'
+      ],
+      tasks: ['default']
+    }
+
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['devUpdate', 'jshint', 'sass', 'autoprefixer', 'cssmin']);
+
+};
